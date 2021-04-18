9f647a4d2267c97d75c43ae5042cd6ed3033bf38
Prashant Dahal <prashantdah41@gmail.com>
2021-04-17 20:39:30 -0400

    Create Room Data Access Object and create Room database

--- app/src/main/java/com/example/contacts/Models/ContactDao.java
+++ app/src/main/java/com/example/contacts/Models/ContactDao.java
@@ -1,4 +1,29 @@
 package com.example.contacts.Models;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
 public interface ContactDao {
+    @Insert
+    void insert(Contact contact);
+
+    @Update
+    void update(Contact contact);
+
+    @Delete
+    void Delete(Contact contact);
+
+    @Query("DELETE FROM contact_table")
+    void deleteAllContacts();
+
+    @Query("SELECT * FROM contact_table ORDER BY first_name ASC")
+    LiveData<List<Contact>> getAllContacts();
+
 }
--- app/src/main/java/com/example/contacts/Models/ContactDatabase.java
+++ app/src/main/java/com/example/contacts/Models/ContactDatabase.java
@@ -1,4 +1,23 @@
 package com.example.contacts.Models;
 
-public class ContactDatabase {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = Contact.class, version = 1)
+public abstract class ContactDatabase extends RoomDatabase {
+    private static ContactDatabase instance;
+
+    public abstract ContactDao contactDao();
+
+    public static synchronized ContactDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(), ContactDatabase.class, "contact_database")
+                        .fallbackToDestructiveMigration()
+                        .build();
+        }
+        return instance;
+    }
 }
