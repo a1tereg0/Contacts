418c2348976aaf78eb226f97d23a0586bc8fcaf4
Prashant Dahal <prashantdah41@gmail.com>
2021-04-23 23:52:29 -0400

    Unresolved changes

--- app/src/main/java/com/example/contacts/MainActivity.java
+++ app/src/main/java/com/example/contacts/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ContactViewModal contactViewModal;
 
-    @Override
+    @Override   
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
